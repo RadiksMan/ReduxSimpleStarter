854867b575acc0a127cb0106dc4256109eccce02
Rodion Ponomarenko <radion91@gmail.com>
2017-09-08 17:15:51 +0300

    no message

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function(){
+    return [
+        {title: "Javascript",pages:101},
+        {title: "Harry Potter",pages:39},
+        {title: "The dart tower",pages:1123312}
+    ]
+}
