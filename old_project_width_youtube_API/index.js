9a723846f62dec536444166001b85f6afbdd565e
Rodion Ponomarenko <radion91@gmail.com>
2017-09-08 12:29:16 +0300

    add old version - width youtube API

---
+++ old_project_width_youtube_API/index.js
@@ -0,0 +1,57 @@
+import _ from 'lodash';
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = `AIzaSyCShZW-Nc_ieJ_eP69rb-_P5iZ3zemvqN8`;
+
+
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            videos:[],
+            selectedVideo:null
+        };
+
+        this.videoSearch('SOAD');
+
+    }
+
+    videoSearch(term){
+
+        YTSearch({
+            key:API_KEY,
+            term:term},
+            (videos) => {
+                this.setState({
+                    videos,
+                    selectedVideo:videos[0]
+                });
+        });
+    }
+
+    render(){
+
+        const vedeoSearch = _.debounce((term) => {this.videoSearch(term)},300)
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={vedeoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}
+                />
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.container'))
